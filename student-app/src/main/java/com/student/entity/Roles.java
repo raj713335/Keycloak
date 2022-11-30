05c4eedae56c8adc30d12b6a67edbbd1ea90dddc
ARNAB DAS <raj713335@gmail.com>
2022-12-01 01:54:30 +0530

    Login Page and Spring Security

---
+++ student-app/src/main/java/com/student/entity/Roles.java
@@ -0,0 +1,43 @@
+package com.student.entity;
+
+import jakarta.persistence.*;
+
+@Table(name="roles")
+@Entity
+public class Roles {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
